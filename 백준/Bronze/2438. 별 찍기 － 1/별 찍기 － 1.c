de4d9b931c09248cd43fd382d38a63c986d265fc
lim58 <128462476+lim58@users.noreply.github.com>
2023-03-29 23:18:18 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int num,i,j;
+	scanf("%d", &num);
+
+	for (i = 1; i <= num; i++)
+	{
+		for (j = 1; j <= i; j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}
