95dea9add83be2c6f1e1959920adb93ab2f4bccc
lim58 <128462476+lim58@users.noreply.github.com>
2023-04-23 22:42:16 +0900

    [Bronze V] Title: A+B - 5, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int a, b;
+	while (1)
+	{
+		scanf("%d %d",&a,&b);
+		if (a==0 && b==0) break;
+		printf("%d\n", a + b);
+	}
+
+	return 0;
+}
