c6973b9a689b1a9d501de99b41266ff5f8b94f66
lim58 <128462476+lim58@users.noreply.github.com>
2024-02-13 23:55:12 +0900

    [level 0] Title: 각도기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/120829. 각도기/각도기.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int solution(int angle) {
+    int answer = 0;
+    
+    if ((0 < angle) && (angle < 90)) answer = 1;
+    if (angle == 90) answer = 2;
+    if ((90 < angle) && (angle < 180)) answer = 3;
+    if (angle == 180) answer = 4;
+    
+    return answer;
+}
