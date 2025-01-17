bd3798dfaabb3a69d8bd0d96ce210578bf272bfc
lim58 <128462476+lim58@users.noreply.github.com>
2025-01-17 22:26:50 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.c
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.c
@@ -6,19 +6,17 @@ int main() {
 
   int dp[15][15] = { 0 };
 
-  for(int i=0; i<=14; i++) {
-    dp[0][i] = i;
+  for (int j = 1; j <= 14; j++) {
+    dp[0][j] = j;
   }
 
-  for(int i=1; i<=14; i++) {
-    for(int j=1; j<=14; j++) {
-      for(int k=1; k<=j; k++) {
-        dp[i][j] += dp[i-1][k];
-      }
+  for (int i = 1; i <= 14; i++) { 
+    for (int j = 1; j <= 14; j++) {
+      dp[i][j] = dp[i-1][j] + dp[i][j-1];
     }
   }
-  
-  while(N--) {
+
+  while (N--) {
     scanf("%d %d", &k, &n);
     printf("%d\n", dp[k][n]);
   }
