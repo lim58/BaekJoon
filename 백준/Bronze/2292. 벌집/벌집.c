5e9e9a82d71d8d6166d8182f9c8972a55012bfb6
lim58 <128462476+lim58@users.noreply.github.com>
2025-01-31 10:36:54 +0900

    [Bronze II] Title: 벌집, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/2292. 벌집/벌집.c
+++ 백준/Bronze/2292. 벌집/벌집.c
@@ -1,22 +1,15 @@
 #include <stdio.h>
 
 int main() {
-  int N;
+  int N, layer=1, hap=2;
   scanf("%d", &N);
-  
-  int hap=1;
-  for(int i=1; ;i++) {
-    if(N == 1) {
-      printf("1");
-      break;
-    }
 
-    hap += i * 6;
-    if(N <= hap) {
-      printf("%d", i + 1);
-      break;
-    }
+  while(N >= hap) {
+    hap += layer * 6;
+    layer++;
   }
+
+  printf("%d", layer);
   
   return 0;
 }
