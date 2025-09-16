bca8a86f60a01508dfb176a7d12f195ca3fda233
lim58 <128462476+lim58@users.noreply.github.com>
2025-09-17 01:47:11 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+bool solution(string s) {
+    
+    if(!(s.size() == 4 || s.size() == 6)) return false;
+    
+    for (int i=0; i<s.size(); i++) {
+        if (s[i] < '0' || s[i] > '9') return false;
+    }
+    
+    return true;
+}
