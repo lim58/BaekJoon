04587c6a555a99af11d33d56b657d75a0dd363ee
lim58 <128462476+lim58@users.noreply.github.com>
2025-09-16 20:01:41 +0900

    [level 1] Title: [PCCE 기출문제] 9번 / 지폐 접기, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/1/340199. ［PCCE 기출문제］ 9번 ／ 지폐 접기/README.md
+++ 프로그래머스/1/340199. ［PCCE 기출문제］ 9번 ／ 지폐 접기/README.md
@@ -1,10 +1,10 @@
 # [level 1] [PCCE 기출문제] 9번 / 지폐 접기 - 340199 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/340199) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/340199#) 
 
 ### 성능 요약
 
-메모리: 4.16 MB, 시간: 0.01 ms
+메모리: 4.14 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 16일 19:58:17
+2025년 09월 16일 20:01:39
 
 ### 문제 설명
 
--- 프로그래머스/1/340199. ［PCCE 기출문제］ 9번 ／ 지폐 접기/［PCCE 기출문제］ 9번 ／ 지폐 접기.cpp
+++ 프로그래머스/1/340199. ［PCCE 기출문제］ 9번 ／ 지폐 접기/［PCCE 기출문제］ 9번 ／ 지폐 접기.cpp
@@ -6,41 +6,34 @@ using namespace std;
 int solution(vector<int> wallet, vector<int> bill) {
     int answer = 0;
     
-    int small = std::min(bill[0], bill[1]);
-int large = std::max(bill[0], bill[1]);
-    
-    while (std::min(bill[0], bill[1]) > std::min(wallet[0], wallet[1]) || std::max(bill[0], bill[1]) > std::max(wallet[0], wallet[1])) {
-        if(bill[0] > bill[1]) {
-            bill[0] /= 2;
-        } else {
-            bill[1] /= 2;
-        }
-        
-        answer++;
-    }
-    
-//     int max = bill[0] < bill[1] ? bill[1] : bill[0];
-//     int min = bill[0] < bill[1] ? bill[0] : bill[1];
-    
-//     while(1) {
-//         if(wallet[0] >= max && wallet[1] >= min) {
-//             break;
+//     while (std::min(bill[0], bill[1]) > std::min(wallet[0], wallet[1]) || std::max(bill[0], bill[1]) > std::max(wallet[0], wallet[1])) {
+//         if(bill[0] > bill[1]) {
+//             bill[0] /= 2;
+//         } else {
+//             bill[1] /= 2;
 //         }
-//         else if (wallet[1] >= max && wallet[0] >= min) {
-//             break;
-//         } 
-//         else {
-//             max /= 2;
-//             answer++;
-            
-//             if (max < min) {
-//                 int temp = min;
-//                 min = max < min ? max : min;
-//                 max = temp;
-//             }
-
-//         }  
+        
+//         answer++;
 //     }
+    
+    int max = bill[0] < bill[1] ? bill[1] : bill[0];
+    int min = bill[0] < bill[1] ? bill[0] : bill[1];
+    
+    while(1) {
+        if(wallet[0] >= max && wallet[1] >= min || wallet[1] >= max && wallet[0] >= min) {
+            break;
+        } else {
+            max /= 2;
+            answer++;
+            
+            if (max < min) {
+                int temp = min;
+                min = max < min ? max : min;
+                max = temp;
+            }
+
+        }  
+    }
     
     return answer;
 }
