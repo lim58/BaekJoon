fad509e58154b59c9e10368b0ba2db8f2fdbac8f
lim58 <128462476+lim58@users.noreply.github.com>
2025-09-16 21:53:00 +0900

    [level 1] Title: 평균 구하기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+double solution(vector<int> arr) {
+    double answer = 0;
+    
+    for (int i=0; i<arr.size(); i++) {
+        answer += arr[i];
+    }
+    
+    answer /= arr.size();
+    
+    return answer;
+}
